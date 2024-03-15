7d7f986232d04dd22918d5e9efe2568c57daa549
ckcool6 <rzdtjb@gmail.com>
2024-03-16 03:03:20 +0800

    avl

--- search.c
+++ search.c
@@ -92,3 +92,66 @@ Status insert_bst(BinaryTree *T, ElemType e)
 }
 
 // 删除
+Status delete(BinaryTree *p)
+{
+    if (!(*p)->Rchild)
+    {
+        BinaryTree q = p;
+        p = (*p)->Lchild;
+        free(q);
+    }
+    else if (!(*p)->Lchild)
+    {
+        BinaryTree q = p;
+        p = (*p)->Rchild;
+        free(q);
+    }
+    else // 左右子树均不为空
+    {
+        BinaryTree q = p;
+        BinaryTree s = (*p)->Lchild;
+        while (s->Rchild)
+        {
+            q = s;
+            s = s->Rchild;
+        } // 转左，然后向右走到头
+
+        (*p)->data = s->data; // s指向被删节点的前驱
+
+        if (q != p)
+        {
+            // 重接右子树
+            q->Rchild = s->Lchild;
+        }
+        else
+        { // 重接左子树
+            q->Lchild = s->Lchild;
+        }
+        free(s);
+    }
+}
+
+Status delete_bst(BinaryTree *T, KeyType key)
+{
+    if (!T)
+    {
+        return -1;
+    }
+    else
+    {
+        if (EQ(key, (*T)->data.key))
+        {
+            return delete (&T);
+        }
+        else if (LT(key, (*T)->data.key))
+        {
+            /* code */
+            return delete_bst((*T)->Lchild, key);
+        }
+        else
+        {
+            return delete_bst((*T)->Rchild, key);
+        }
+    }
+}
+
